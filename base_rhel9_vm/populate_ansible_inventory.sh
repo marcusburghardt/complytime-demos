874c771b97fdf266c92a4f7b48d7648b0a4fb9ab
Marcus Burghardt <maburgha@redhat.com>
2025-05-20 17:37:42 +0200

    feat: include SSH host key automatically
    Once a VM is provisioned, is connecting to it via SSH without using the
    vagrant command, ssh will ask to accept the host key. SSH is used for
    many tests, so the host key is automatically included after provisioning
    the VM.
    
    Signed-off-by: Marcus Burghardt <maburgha@redhat.com>

--- base_rhel9_vm/populate_ansible_inventory.sh
+++ base_rhel9_vm/populate_ansible_inventory.sh
@@ -4,7 +4,12 @@
 # Get Vagrant SSH configuration
 VAGRANT_SSH_CONFIG=$(vagrant ssh-config)
 
-# Extract the name and IP address of the VM
+# Add the VM's IP address to the known_hosts file to avoid SSH prompts
+IP=$(echo "$VAGRANT_SSH_CONFIG" | awk '/HostName/ {print $2}')
+ssh-keyscan -H "$IP" >> ~/.ssh/known_hosts 2>/dev/null
+echo "IP address $IP added to known_hosts file."
+
+# Populate the Ansible inventory file with the VM's SSH configuration
 echo "[demo_vm]" > ../base_ansible_env/ansible_inventory
 echo "$VAGRANT_SSH_CONFIG" | awk '
 /Host / {host=$2}
@@ -15,7 +20,6 @@ END {print host, "ansible_host="hostname, "ansible_user=ansible", "ansible_ssh_p
 echo "Inventory file generated successfully!"
 cat ../base_ansible_env/ansible_inventory
 
-echo ""
 echo "You can connect to the VMs using the following command:"
 echo "$VAGRANT_SSH_CONFIG" | awk '
 /HostName/ {hostname=$2}
